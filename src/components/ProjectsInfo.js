738c73672aebbab49cdefcc773cd2cb18c51322e
unknown <javimorenomontejo@gmail.com>
2021-11-25 19:46:38 +0100

    add projects

--- src/components/ProjectsInfo.js
+++ src/components/ProjectsInfo.js
@@ -1,5 +1,87 @@
 const projects = [
     {
+        title: "World War II in Europe",
+        startDate: "10 - 2021",
+        images: [
+                    "https://i.imgur.com/lmLBg2y.png",
+                    "https://i.imgur.com/dm7s1HG.png",
+                    "https://i.imgur.com/o4FnAR0.png",
+                    "https://i.imgur.com/aqOh3Mr.png",
+                    "https://i.imgur.com/ttVgImp.png",
+                    "https://i.imgur.com/Ah4azTu.png",
+                    "https://i.imgur.com/9jU4FAD.png",
+                    "https://i.imgur.com/5AdJTX2.png",
+                    "https://i.imgur.com/K8z4EfN.png",
+                    "https://i.imgur.com/ZJ3yGcq.png",
+                    "https://i.imgur.com/BnZJOFc.png",
+                    "https://i.imgur.com/tlSHOjW.gif"
+        ],
+        url: "https://github.com/javike19/WW2-angular-app",
+        technologies: [
+            {
+                class: "https://docs.angular.lat/assets/images/logos/angular/angular.png",
+                        name: "Angular"
+            }
+        ]
+    },
+    {
+        title: "Time Project",
+        startDate: "11 - 2021",
+        images: [
+                    "https://i.imgur.com/JX7Ogpx.png",
+                    "https://i.imgur.com/CHKEMXk.png",
+                    "https://i.imgur.com/rVWc2un.gif"
+                   
+        ],
+        url: "https://github.com/javike19/Time-Project-React-Router",
+        technologies: [
+            {
+                class: "https://cdn-icons-png.flaticon.com/512/1126/1126012.png",
+                name: "React"
+            }
+        ]
+    },
+    {
+        title: "Guacamole Game",
+        startDate: "10 - 2021",
+        images: [
+                    "https://i.imgur.com/gygmXXY.png",
+                    "https://i.imgur.com/veRPhUm.gif"
+                
+        ],
+        url: "https://javike19.github.io/Guacamole-Game/",
+        technologies: [
+            {
+                class: "https://cdn-icons-png.flaticon.com/512/1051/1051277.png",
+                        name: "HTML5"
+            },
+            {
+                class: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+                name: "CSS3"
+            },
+            {
+                class: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+                name: "JavaScript"
+            }
+        ]
+    },
+    {
+        title: "Netflix copy",
+        startDate: "10 - 2021",
+        images: [
+                    "https://i.imgur.com/glKdAZI.png",
+                    "https://i.imgur.com/0nHrTkO.png"
+                
+        ],
+        url: "https://github.com/javike19/netflix-app",
+        technologies: [
+            {
+                class: "https://docs.angular.lat/assets/images/logos/angular/angular.png",
+                        name: "Angular"
+            }
+        ]
+    },
+    {
         title: "GTA San Andreas FanWeb",
         startDate: "10 - 2021",
         images: [
@@ -48,71 +130,9 @@ const projects = [
             }
         ]
     },
-    {
-        title: "Guacamole Game",
-        startDate: "10 - 2021",
-        images: [
-                    "https://i.imgur.com/gygmXXY.png",
-                    "https://i.imgur.com/veRPhUm.gif"
-                
-        ],
-        url: "https://javike19.github.io/Guacamole-Game/",
-        technologies: [
-            {
-                class: "https://cdn-icons-png.flaticon.com/512/1051/1051277.png",
-                        name: "HTML5"
-            },
-            {
-                class: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
-                name: "CSS3"
-            },
-            {
-                class: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
-                name: "JavaScript"
-            }
-        ]
-    },
-    {
-        title: "Netflix copy",
-        startDate: "10 - 2021",
-        images: [
-                    "https://i.imgur.com/glKdAZI.png",
-                    "https://i.imgur.com/0nHrTkO.png"
-                
-        ],
-        url: "https://github.com/javike19/netflix-app",
-        technologies: [
-            {
-                class: "https://docs.angular.lat/assets/images/logos/angular/angular.png",
-                        name: "Angular"
-            }
-        ]
-    },
-    {
-        title: "World War II in Europe",
-        startDate: "10 - 2021",
-        images: [
-                    "https://i.imgur.com/lmLBg2y.png",
-                    "https://i.imgur.com/dm7s1HG.png",
-                    "https://i.imgur.com/o4FnAR0.png",
-                    "https://i.imgur.com/aqOh3Mr.png",
-                    "https://i.imgur.com/ttVgImp.png",
-                    "https://i.imgur.com/Ah4azTu.png",
-                    "https://i.imgur.com/9jU4FAD.png",
-                    "https://i.imgur.com/5AdJTX2.png",
-                    "https://i.imgur.com/K8z4EfN.png",
-                    "https://i.imgur.com/ZJ3yGcq.png",
-                    "https://i.imgur.com/BnZJOFc.png",
-                    "https://i.imgur.com/tlSHOjW.gif"
-        ],
-        url: "",
-        technologies: [
-            {
-                class: "https://docs.angular.lat/assets/images/logos/angular/angular.png",
-                        name: "Angular"
-            }
-        ]
-    },
+   
+    
+    
 ]
 
 export default projects
